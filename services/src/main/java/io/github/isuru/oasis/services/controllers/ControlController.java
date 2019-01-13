a6b0e3ac156f8453959ab92b725aa6541ad967eb
isuru89 <uisurumadushanka89@gmail.com>
2019-01-13 17:47:10 +0530

    implemented control (formally lifecycle) controller

---
+++ services/src/main/java/io/github/isuru/oasis/services/controllers/ControlController.java
@@ -0,0 +1,49 @@
+package io.github.isuru.oasis.services.controllers;
+
+import io.github.isuru.oasis.services.dto.StatusResponse;
+import io.github.isuru.oasis.services.services.ILifecycleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/control")
+public class ControlController {
+
+    @Autowired
+    private ILifecycleService lifecycleService;
+
+
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping("/game/{gameId}/start")
+    @ResponseBody
+    public StatusResponse startGame(@PathVariable("gameId") long gameId) throws Exception {
+        return new StatusResponse(lifecycleService.start(gameId));
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping("/game/{gameId}/stop")
+    @ResponseBody
+    public StatusResponse stopGame(@PathVariable("gameId") long gameId) throws Exception {
+        return new StatusResponse(lifecycleService.stop(gameId));
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping("/challenge/{challengeId}/start")
+    @ResponseBody
+    public StatusResponse startChallenge(@PathVariable("challengeId") long challengeId) throws Exception {
+        return new StatusResponse(lifecycleService.startChallenge(challengeId));
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping("/challenge/{challengeId}/stop")
+    @ResponseBody
+    public StatusResponse stopChallenge(@PathVariable("challengeId") long challengeId) throws Exception {
+        return new StatusResponse(lifecycleService.stop(challengeId));
+    }
+}
