ac6380328722b38612f651789c0afd0ae6ff2a42
isuru89 <uisurumadushanka89@gmail.com>
2020-09-12 08:25:33 +0530

    tests fixed after badges impl #65

--- engine/src/test/java/io/github/oasis/engine/OasisEngineTest.java
+++ engine/src/test/java/io/github/oasis/engine/OasisEngineTest.java
@@ -118,7 +118,6 @@ public class OasisEngineTest {
         try (DbContext db = dbPool.createContext()) {
             db.allKeys("*").forEach(db::removeKey);
 
-            metadataSupport.addUser(new UserObject(0, "Malanie Seans", "melanie@oasis.io"));
             metadataSupport.addUser(new UserObject(1, "Jakob Floyd", "jakob@oasis.io"));
             metadataSupport.addUser(new UserObject(2, "Thierry Hines", "thierry@oasis.io"));
             metadataSupport.addUser(new UserObject(3, "Ray Glenn", "ray@oasis.io"));
